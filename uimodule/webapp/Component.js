0fcb62966a22d371575e154079a379395438d7a8
Jose Lopez <josellopezf@gmail.com>
2022-07-05 12:20:51 -0400

    PDF funcionando

--- uimodule/webapp/Component.js
+++ uimodule/webapp/Component.js
@@ -1,10 +1,12 @@
-sap.ui.define(
-    ["sap/ui/core/UIComponent", "sap/ui/Device", "SCO/BTP/ListadoPicking/model/models"],
+sap.ui.define([
+    "sap/ui/core/UIComponent", 
+    "sap/ui/Device", 
+    "SCO/BTP/ListadoPicking/model/models",  
     /**
      * @param {typeof sap.ui.core.UIComponent} UIComponent
      * @param {typeof sap.ui.Device} Device
      */
-    function (UIComponent, Device, models) {
+    ],function (UIComponent, Device, models) {
         "use strict";
 
         return UIComponent.extend("SCO.BTP.ListadoPicking.Component", {
